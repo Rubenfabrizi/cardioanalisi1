9d990c1a3ad8c9453f1b2dbc9c31a285d4b0d5be
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-11 09:41:16 +0200

    metodo calcola frequenza

---
+++ equazionilybrary/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equazionilybrary
+{
+    public class Class1
+    {
+        
+        public static int Calcfrecquenza(int eta)
+        {
+            
+            int freq =  220 - eta;
+            return freq;
+        }
+    }
+}
